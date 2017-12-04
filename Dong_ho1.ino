58fec19c9f93d9ee3a9d6c051146718e219eae47
TachibanaKanade99 <sad361975@gmail.com>
2017-12-04 22:06:24 +0700

    Add file

--- Dong_ho1.ino
+++ Dong_ho1.ino
@@ -1,6 +1,8 @@
 #include <DS3231.h>
 DS3231 rtc(SDA,SCL);
 int led = 13;
+String inString = "";
+int h;
 
 void setup() {
 	Serial.print(9600);
@@ -8,6 +10,8 @@ void setup() {
 	pinMode(led, OUTPUT);
 	Serial.println("\n\nString  substring():");
 	Serial.println();
+  Serial.println("\n\nString toInt():");
+  Serial.println();
   rtc.setDOW(WEDNESDAY);
 	rtc.setTime("19", "04", "00");
 	rtc.setDate("12", "01", "2017");
@@ -18,20 +22,19 @@ void setup() {
  	Serial.print(" ");
   String b =  rtc.getDateStr(); 
  	Serial.println(b);
-  String c = rtc,getTimeStr();
+  String c = rtc.getTimeStr();
  	Serial.println(c);
  	Serial.println();
 
-  n = (int) c.substring(0,2);
-  if n > 12 ) {
+    String n = c.substring(1,2);
+    if (n == '\n') {
+       h =  Serial.println(inString.toInt());   
+    }
+    if ( h >12 ){
     digitalWrite(led, HIGH);
-    else
-    digitalWrite(led, ;LOW);
-  }
-
- 	if (isnan(rtc.getDOWStr) || isnan(rtc.getDateStr) || isnan(rtc.getTimeStr)) {
- 		Serial.println("Lỗi không đọc được dữ liệu");
- 		return;
- 	}
+    }
+    else {
+    digitalWrite(led, LOW);
+    }   
  	delay(1000);
  }
